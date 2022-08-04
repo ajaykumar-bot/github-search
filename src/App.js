ce74004c816a7d59e3866af0849b2eb3d4af82f6
unknown <Ajay Kumar>
2022-08-04 18:24:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import React, { Component } from 'react';
 import Navbar from './component/layout/Navbar';
-import Users from './users/Users';
+import Users from './component/users/Users';
+import Search from './component/users/Search';
 import axios from 'axios';
 
 class App extends Component {
@@ -11,21 +12,41 @@ class App extends Component {
 		loading: false,
 	};
 
-	async componentDidMount() {
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+
+	// 	const res = await axios.get('https://api.github.com/users');
+
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	// search github users
+	searchUsers = async text => {
 		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}`
+		);
 
-		const res = await axios.get('https://api.github.com/users');
-
-		this.setState({ users: res.data, loading: false });
-	}
+		this.setState({ users: res.data.items, loading: false });
+	};
+	//clear users
+	clearUsers = () => {
+		this.setState({ users: [], loading: false });
+	};
 
 	render() {
+		const { users, loading } = this.state;
 		// const name = 'JOhn Doe';
 		return (
 			<div className='App'>
 				<Navbar />
 				<div className='container'>
-					<Users loading={this.state.loading} users={this.state.users} />
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+					/>
+					<Users loading={loading} users={this.state.users} />
 				</div>
 			</div>
 		);
